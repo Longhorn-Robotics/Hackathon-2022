b886fde372525190d97256943885f650306c4414
Saragon4005 <domikakeszei@gmail.com>
2022-03-20 14:19:43 -0700

    add basic endpoint

--- login.py
+++ login.py
@@ -1,8 +1,15 @@
+from flask import jsonify, request
+from app import app
 import sqlite3
 
 con = sqlite3.connect('backend.db')
 cur = con.cursor()
 
 cur.execute(
-    "CREATE TABLE users (id INT PRIMARY KEY, email TINYTEXT, name TEXT, password TEXT, discord_id INT, canvas_token TEXT)"
+    "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY, email TINYTEXT UNIQUE, name TEXT, password TEXT, discord_id INT, canvas_token TEXT)"
 )
+
+
+@app.route("/v1/login", methods=["CREATE"])
+def create_user():
+    return (jsonify(request.get_json()))
